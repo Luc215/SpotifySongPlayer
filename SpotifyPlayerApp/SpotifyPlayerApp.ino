4b1b9278d398b704fb764e42e7cf4585b38afad8
Luc215 <lucaslyubenko9@gmail.com>
2026-06-11 23:49:50 -0400

    Add initial SpotifyPlayerApp Arduino sketch
    Added a new SpotifyPlayerApp.ino skeleton for an ESP32-based Spotify player. The file includes required libraries and placeholders for SSID and password.

---
+++ SpotifyPlayerApp/SpotifyPlayerApp.ino
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Wifi.h>
+#include <SpotifyEsp32.h>
+#include <SPI.h>
+
+const char* SSID = "Insert_SSID_HERE";
+const char* PASSWORD = "INSERT_PASS_HERE"
+void setup() {
+  Serial.begin(115200)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

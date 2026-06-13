05893d77a484b71d968a92fead884546e1220fe1
Luc215 <lucaslyubenko9@gmail.com>
2026-06-12 22:43:08 -0400

    Inititialized TFT display and made Wifi and Spotify API connections

--- SpotifyPlayerApp/SpotifyPlayerApp.ino
+++ SpotifyPlayerApp/SpotifyPlayerApp.ino
@@ -6,14 +6,50 @@
 #include <SpotifyEsp32.h>
 #include <SPI.h>
 
+#define TFT_CS 1
+#define TFT_RST 2
+#define TFT_DC 3
+#define TFT_SCLK 4
+#define TFT_MOSI 5
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
 const char* SSID = "Insert_SSID_HERE";
 const char* PASSWORD = "INSERT_PASS_HERE"
+const char* CLIENT_ID = "INSERT_CLIENT_ID";
+const char* CLIENT_SECRET = "INSERT_CLIENT_SECRET"
+
 void setup() {
   Serial.begin(115200)
+
+  tft.initR(INITR_BLACKTAB);
+  tft.setRotation(1);
+  Serial.println("TFT Display Initialized!");
+  tft.fillScreen(ST77XX_BLACK);
+
+  Wifi.begin(SSID, PASSWORD);
+  Serial.print("Attempting to connect to wifi...");
+  while(Wifi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected To Wifi!\n");
+
+  tft.setCursor(0.0); // cursor to the top left
+  tft.write(Wifi.localIP().toString().c_str()); // print IP to screen
+
+  Spotify spotifyPlayer(CLIENT_ID, CLIENT_SECRET);
+
+  spotifyPlayer.begin();
+  while(!spotifyPlayer.is_auth()){
+    delay(1000);
+    spotifyPlayer.handle_Client();
+  }
+  Serial.println("Connected to Spotify!");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }

01319c85aac0e1d769abfbe8b175a827198c6146
Luc215 <lucaslyubenko9@gmail.com>
2026-06-25 14:32:47 -0500

    Small syntax fix

--- SpotifyPlayerApp/SpotifyPlayerApp.ino
+++ SpotifyPlayerApp/SpotifyPlayerApp.ino
@@ -83,7 +83,7 @@ void loop() {
 bool tftOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap){
   if (y >= tft.height()) return 0;
 
-  tft.pushimage(x,y,w,h,bitmap);
+  tft.pushImage(x,y,w,h,bitmap);
   return 1;
 }
 
